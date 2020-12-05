e3bc57866a0e0e46876b69a53a8737e6d9c508cb
PurkkaKoodari <purkka@purkka.codes>
2020-12-05 04:28:24 +0200

    Update pins for Simon Says

--- module/simon.h
+++ module/simon.h
@@ -11,16 +11,16 @@
 
 #define MODULE_NAME "Simon Says"
 
-#define LED_BLUE_PIN A0
-#define LED_YELLOW_PIN 4
-#define LED_GREEN_PIN A1
-#define LED_RED_PIN A2
-#define BUTTON_BLUE_PIN 7
-#define BUTTON_YELLOW_PIN 6
-#define BUTTON_GREEN_PIN 5
-#define BUTTON_RED_PIN 8
+#define LED_BLUE_PIN 10
+#define LED_YELLOW_PIN 6
+#define LED_GREEN_PIN 5
+#define LED_RED_PIN 9
+#define BUTTON_BLUE_PIN 3
+#define BUTTON_YELLOW_PIN 4
+#define BUTTON_GREEN_PIN A4
+#define BUTTON_RED_PIN 7
 
-#define STRIKE_LED_PIN A5
-#define SOLVE_LED_PIN A4
+#define STRIKE_LED_PIN A0
+#define SOLVE_LED_PIN A1
 
 #endif
